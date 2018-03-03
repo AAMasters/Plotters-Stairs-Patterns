eb93e29ef1085b32deba96c234b55e6f29e74081
Luis Fernando Molina <Luis-Fernando-Molina@users.noreply.github.com>
2018-03-03 19:07:14 +0100

    Final Touches

--- CandleStairs.js
+++ CandleStairs.js
@@ -133,13 +133,8 @@
 
     function onDailyFileLoaded(event) {
 
-        if (event.currentValue === event.totalValue) {
+        recalculate();
 
-            /* This happens only when all of the files in the cursor have been loaded. */
-
-            recalculate()
-
-        }
     }
 
     function draw() {
